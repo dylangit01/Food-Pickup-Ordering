f9fb4d77a424e469c0f41f0b947f7b8f6431ac9b
Dylan <dcren2010@gmail.com>
2021-05-02 08:55:20 -0400

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,15 @@
+$("document").ready(() => {
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/users",
+  }).done((users) => {
+    for (user of users.users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });
+});
+
+})
+
+
